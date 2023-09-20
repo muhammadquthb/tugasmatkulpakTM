f53acf6f139ddbfcf2a503311ef672ef30fe0c52
muhammadquthb <145528649+muhammadquthb@users.noreply.github.com>
2023-09-20 12:26:23 +0700

    Update BilanganGanjilGenap.java

--- BilanganGanjilGenap/src/bilanganganjilgenap/BilanganGanjilGenap.java
+++ BilanganGanjilGenap/src/bilanganganjilgenap/BilanganGanjilGenap.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package bilanganganjilgenap;
+package tugasbilanganganjilgenap;
 import java.util.Scanner;
 /**
  *
  * @author acer
  */
-public class BilanganGanjilGenap {
+public class tugasBilanganGanjilGenap {
 
     /**
      * @param args the command line arguments
